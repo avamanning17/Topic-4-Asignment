ed28c02595a6424f95c2f8dc85cd3a142e2ac24f
Ava Manning <362579849@GoTVDSB.ca>
2025-09-24 09:34:44 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Topic_4_Asignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            //string item1, item2;
+            //double price1, price2;
+
+            //Console.WriteLine("What is your first item?");
+
+
+            //price = 7.50;
+            //badPrice = 5.00;
+
+            ////Cash Register
+            //Console.WriteLine("Our best selling fruit is " + fruit + " and it is " + price.ToString("C") + ".");
+
+            //Console.WriteLine("What is your worst selling fruit and how much is it?");
+            //Console.WriteLine("Our worst selling fruit is " + badFruit + " and it is " + badPrice.ToString("C") + ".");
+        }
+    }
+}
