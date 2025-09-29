07cc7a47894368530277f5055e6fae28b7e67bac
Ava Manning <362579849@GoTVDSB.ca>
2025-09-29 09:32:49 -0400

    Working

--- Program.cs
+++ Program.cs
@@ -1,27 +1,43 @@
-﻿namespace Topic_4_Asignment
+﻿using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace Topic_4_Asignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //String name;
+            //int age;
+            //Double money;
+
+            //Console.WriteLine("Hello what is your name?");
+            //name = Console.ReadLine();
+            //Console.WriteLine();
+            //Console.WriteLine("Hello " + name + ("!") + " How old are you?");
+            //int.TryParse(Console.ReadLine(), out age);
+            //Console.WriteLine();
+            //Console.WriteLine("So youre " + age + " eh? Thats not old at all! How much do you make, " + name + ("?"));
+            //Double.TryParse(Console.ReadLine(), out money);
+            //Console.WriteLine();
+            //Console.WriteLine(money.ToString("C") + ("!") + " I hope that's per hour and not per year!");
+
+            String firstName, lastName, login;
+            int grade, idNumber;
+            Double average;
+
+            firstName = Console.ReadLine();
+            lastName = Console.ReadLine();
+            int.TryParse(Console.ReadLine(), out grade);
+            int.TryParse(Console.ReadLine(), out idNumber);
+            Double.TryParse(Console.ReadLine(), out average);
+
+        login: Console.WriteLine("ID: /t/t");
 
 
 
 
-            //string item1, item2;
-            //double price1, price2;
 
-            //Console.WriteLine("What is your first item?");
-
-
-            //price = 7.50;
-            //badPrice = 5.00;
-
-            ////Cash Register
-            //Console.WriteLine("Our best selling fruit is " + fruit + " and it is " + price.ToString("C") + ".");
-
-            //Console.WriteLine("What is your worst selling fruit and how much is it?");
-            //Console.WriteLine("Our worst selling fruit is " + badFruit + " and it is " + badPrice.ToString("C") + ".");
         }
     }
 }
